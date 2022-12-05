0ea8ad4d82900e57787ee13068a02d1aaeff63f0
javivaleiras <javivaleiras95@gmail.com>
2022-12-05 20:01:28 +0100

    Save players start

--- players_scraping/functions.py
+++ players_scraping/functions.py
@@ -9,42 +9,39 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
-import time
+
 
 def get_tables(driver,xpath_table,to_click='//a[@id="next"]',listbox='//dl[@class="listbox right"]//b'):
     all_table = []
     next_page = True
     while next_page:
        
-        try:
-            WebDriverWait(driver, 10)\
-              .until(EC.element_to_be_clickable((By.XPATH,
-                                                  xpath_table)))  
+        WebDriverWait(driver, 1000000000)\
+            .until(EC.element_to_be_clickable((By.XPATH,
+                                                xpath_table)))
 
-            table_columns = driver.find_element_by_xpath(xpath_table)
-            table = table_columns.text
-            all_table.append(table)
-  
-            try:
-                WebDriverWait(driver, 10)\
+        table_columns = driver.find_element_by_xpath(xpath_table)
+        table = table_columns.text
+        all_table.append(table)
+
+        try:
+            WebDriverWait(driver, 1000000000)\
                     .until(EC.element_to_be_clickable((By.XPATH,
-                                                              to_click.replace(' ', '.'))))\
-                        .click()
-            except:
-                WebDriverWait(driver, 10)\
+                                                       to_click.replace(' ', '.'))))\
+                    .click()
+        except:
+            WebDriverWait(driver, 1000000000)\
                     .until(EC.element_to_be_clickable((By.XPATH,
-                                                              to_click.replace(' ', '.'))))\
-                        .click()
-            
-            
-            is_next = driver.find_element_by_xpath(listbox).text
-            is_next = is_next.split('|')[0]
-            page1 = is_next.split('/')[0].split(' ')[1]
-            page2 = is_next.split('/')[1]
-            if(int(page1) == int(page2)):
-                next_page=False
-        except NoSuchElementException:
+                                                       to_click.replace(' ', '.'))))\
+                    .click()
+
+        is_next = driver.find_element_by_xpath(listbox).text
+        is_next = is_next.split('|')[0]
+        page1 = is_next.split('/')[0].split(' ')[1]
+        page2 = is_next.split('/')[1]
+        if(int(page1) == int(page2)):
             next_page = False
+
 
     return all_table
 
--- players_scraping/players_scraper.py
+++ players_scraping/players_scraper.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
 import time
+import math
 from functions import get_tables
 import pandas as pd
 
@@ -48,44 +49,64 @@ WebDriverWait(driver, 5)\
     .click()
 
 # players table
-all_summary = get_tables(driver,'//div[@id="statistics-table-summary"]')
+all_summary = get_tables(driver,'//div[@id="statistics-table-summary"]','//div[@id="statistics-paging-summary"]//a[@id="next"]')
+WebDriverWait(driver, 1000000000)\
+            .until(EC.element_to_be_clickable((By.XPATH,
+                                                '//div[@id="statistics-table-summary"]')))
 
+table_columns = driver.find_element_by_xpath('//div[@id="statistics-table-summary"]')
+table = table_columns.text
+all_summary.append(table)
 #defensive
-WebDriverWait(driver, 10)\
-    .until(EC.element_to_be_clickable((By.XPATH,
-                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-defensive"]'.replace(' ', '.'))))\
-    .click()
+# WebDriverWait(driver, 10)\
+#     .until(EC.element_to_be_clickable((By.XPATH,
+#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-defensive"]'.replace(' ', '.'))))\
+#     .click()
 
 
-all_defensive = get_tables(driver,'//div[@id="statistics-table-defensive"]',
-                           '//div[@id="statistics-paging-defensive"]//a[@id="next"]',
-                           '//div[@id="statistics-paging-defensive"]//dl[@class="listbox right"]//b')
+# all_defensive = get_tables(driver,'//div[@id="statistics-table-defensive"]',
+#                            '//div[@id="statistics-paging-defensive"]//a[@id="next"]',
+#                            '//div[@id="statistics-paging-defensive"]//dl[@class="listbox right"]//b')
 
-#offensive
-WebDriverWait(driver, 10)\
-    .until(EC.element_to_be_clickable((By.XPATH,
-                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-offensive"]'.replace(' ', '.'))))\
-    .click()
+# #offensive
+# WebDriverWait(driver, 10)\
+#     .until(EC.element_to_be_clickable((By.XPATH,
+#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-offensive"]'.replace(' ', '.'))))\
+#     .click()
     
 
-all_offensive = get_tables(driver,'//div[@id="statistics-table-offensive"]',
-                           '//div[@id="statistics-paging-offensive"]//a[@id="next"]',
-                           '//div[@id="statistics-paging-offensive"]//dl[@class="listbox right"]//b')
+# all_offensive = get_tables(driver,'//div[@id="statistics-table-offensive"]',
+#                            '//div[@id="statistics-paging-offensive"]//a[@id="next"]',
+#                            '//div[@id="statistics-paging-offensive"]//dl[@class="listbox right"]//b')
 
-#passing
-WebDriverWait(driver, 5)\
-    .until(EC.element_to_be_clickable((By.XPATH,
-                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-passing"]'.replace(' ', '.'))))\
-    .click()
+# #passing
+# WebDriverWait(driver, 5)\
+#     .until(EC.element_to_be_clickable((By.XPATH,
+#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-passing"]'.replace(' ', '.'))))\
+#     .click()
     
-all_passing = get_tables(driver,'//div[@id="statistics-table-passing"]',
-                           '//div[@id="statistics-paging-passing"]//a[@id="next"]',
-                           '//div[@id="statistics-paging-passing"]//dl[@class="listbox right"]//b')
+# all_passing = get_tables(driver,'//div[@id="statistics-table-passing"]',
+#                            '//div[@id="statistics-paging-passing"]//a[@id="next"]',
+#                            '//div[@id="statistics-paging-passing"]//dl[@class="listbox right"]//b')
+
+
+## Player creation
 
 
 
-
-
-
+for x in all_summary:
+    try:
+        name = x.split('\n')
+        players_num = int(len(name))
+        players_num = math.floor((players_num / 4))
+        # club = x.split('\n').split('\n').split(',')[0]
+        # possiton = x.split('\n').split('\n').split(',').split('\n')[0]
+        # apps = x.split('\n').split('\n').split(',').split('\n').split('(')[0]
+        # mins = x.split('\n').split('\n').split(',').split('\n').split(' ').split(' ')[0]
+    
+        # print(name+" "+club+" "+possiton+" "+apps+" "+mins)
+        print(players_num)
+    except:
+        print("error")
     
     
