25a3005d3086299501d8950b106050d1ffd7ea5b
javivaleiras <javivaleiras95@gmail.com>
2022-12-08 18:28:46 +0100

    Finished scrapper one season

--- players_scraping/functions.py
+++ players_scraping/functions.py
@@ -9,7 +9,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
-
+import pandas as pd
+import math
 
 def get_tables(driver,xpath_table,to_click='//a[@id="next"]',listbox='//dl[@class="listbox right"]//b'):
     all_table = []
@@ -46,3 +47,149 @@ def get_tables(driver,xpath_table,to_click='//a[@id="next"]',listbox='//dl[@clas
     return all_table
 
 
+def create_df_summary(all_summary):
+       summary_player = pd.DataFrame(columns=['name', 'club', 'position', 'apps', 'mins',
+                                     'goals', 'assists', 'yel', 'red', 'shots', 'ps%', 'aerials_won', 'motm', 'rating'])
+       for x in all_summary:
+             try:
+                  players = x.split('\n')
+                  players_num = int(len(players))
+                  players_num = math.floor((players_num / 4))
+                  name_index = 3
+                  for i in range(0, players_num):
+                      name = players[name_index].strip()
+                      club = players[name_index + 1].split(',')[0].strip()
+                      position = players[name_index + 1].split(',')[-1].strip()
+                      stats = players[name_index + 2].split(' ')
+                      apps = stats[0].strip()
+                      mins = stats[1].strip()
+                      goals = stats[2].strip()
+                      assists = stats[3].strip()
+                      yel = stats[4].strip()
+                      red = stats[5].strip()
+                      spg = stats[6].strip()
+                      ps = stats[7].strip()
+                      aerials = stats[8].strip()
+                      motm = stats[9].strip()
+                      rating = stats[10].strip()
+
+                      summary_p = {'name': name, 'club': club, 'position': position, 'apps': apps, 'mins': mins, 'goals': goals,
+                          'assists': assists, 'yel': yel, 'red': red, 'shots': spg, 'ps%': ps, 'aerials_won': aerials, 'motm': motm, 'rating': rating}
+                      summary_player = summary_player.append(
+                          summary_p, ignore_index=True)
+
+                      name_index += 4
+
+             except Exception as e:
+                  print(e)
+
+       return summary_player
+   
+def create_df_defensive(all_defensive):
+    defensive_player = pd.DataFrame(columns=['name','club','position','mins','tackles','interceptions','fouls','offsides_won','clearances','dribbled','blocks','own_goals'])
+    for x in all_defensive:
+        try:
+            players = x.split('\n')
+            players_num = int(len(players))
+            players_num = math.floor((players_num / 4))
+            name_index = 3
+            for i in range(0,players_num):
+                name = players[name_index].strip()
+                club = players[name_index + 1].split(',')[0].strip()
+                position = players[name_index + 1].split(',')[-1].strip()
+                stats = players[name_index + 2].split(' ')
+                mins = stats[1].strip()
+                tackles = stats[2].strip()
+                inter = stats[3].strip()
+                fouls = stats[4].strip()
+                offsides = stats[5].strip()
+                clear = stats[6].strip()
+                drb = stats[7].strip()
+                blocks = stats[8].strip()
+                owng = stats[9].strip()
+                
+                
+                defensive_p = {'name':name,'club':club,'position':position,'mins':mins,'tackles':tackles,'interceptions':inter,'fouls':fouls,'offsides_won':offsides,'clearances':clear,'dribbled':drb,'blocks':blocks,'own_goals':owng}
+                defensive_player = defensive_player.append(defensive_p,ignore_index=True)
+                
+                name_index += 4 
+            
+            
+        except Exception as e:
+            print(e)
+    
+    return defensive_player
+
+def create_df_offensive(all_offensive):
+    offensive_player = pd.DataFrame(columns=['name','club','position','mins','key_passes','dribblings','fouled','offsides','dispossed','bad_controls'])
+    for x in all_offensive:
+        try:
+            players = x.split('\n')
+            players_num = int(len(players))
+            players_num = math.floor((players_num / 4))
+            name_index = 3
+            for i in range(0,players_num):
+                name = players[name_index].strip()
+                club = players[name_index + 1].split(',')[0].strip()
+                position = players[name_index + 1].split(',')[-1].strip()
+                stats = players[name_index + 2].split(' ')
+                mins = stats[1].strip()
+                keyp = stats[5].strip()
+                drb = stats[6].strip()
+                fouled = stats[7].strip()
+                off = stats[8].strip()
+                disp = stats[9].strip()
+                unstch = stats[10].strip()
+                
+                
+                offensive_p = {'name':name,'club':club,'position':position,'mins':mins,'key_passes':keyp,'dribblings':drb,'fouled':fouled,'offsides':off,'dispossed':disp,'bad_controls':unstch}
+                offensive_player = offensive_player.append(offensive_p,ignore_index=True)
+                
+                name_index += 4 
+            
+            
+        except Exception as e:
+            print(e)
+    
+    return offensive_player
+
+
+def create_df_passing(all_passing):
+    passing_player = pd.DataFrame(columns=['name','club','position','mins','avg_passes','crosses','long_passes','through_passes'])
+    for x in all_passing:
+        try:
+            players = x.split('\n')
+            players_num = int(len(players))
+            players_num = math.floor((players_num / 4))
+            name_index = 3
+            for i in range(0,players_num):
+                name = players[name_index].strip()
+                club = players[name_index + 1].split(',')[0].strip()
+                position = players[name_index + 1].split(',')[-1].strip()
+                stats = players[name_index + 2].split(' ')
+                mins = stats[1].strip()
+                avgp = stats[4].strip()
+                crosses = stats[6].strip()
+                longb = stats[7].strip()
+                thrb = stats[8].strip()
+              
+                
+                
+                passing_p = {'name':name,'club':club,'position':position,'mins':mins,'avg_passes':avgp,'crosses':crosses,'long_passes':longb,'through_passes':thrb}
+                passing_player = passing_player.append(passing_p,ignore_index=True)
+                
+                name_index += 4 
+            
+            
+        except Exception as e:
+            print(e)
+    
+    return passing_player
+
+
+def merge_all_dfs(summary,defensive,offensive,passing):
+    a = pd.merge(summary,defensive,left_on=['name','club','position','mins'], right_on=['name','club','position','mins'], how='left')
+    b = pd.merge(offensive,passing,left_on=['name','club','position','mins'], right_on=['name','club','position','mins'], how='left')
+    final = pd.merge(a,b,left_on=['name','club','position','mins'], right_on=['name','club','position','mins'], how='left')
+    final = final.drop_duplicates()
+    return final
--- players_scraping/players_scraper.py
+++ players_scraping/players_scraper.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException,NoSuchElementException
-from functions import get_tables
+from functions import get_tables,create_df_summary,create_df_defensive,create_df_offensive,create_df_passing,merge_all_dfs
 from summary import summary
 import time
 import math
@@ -44,87 +44,113 @@ driver.get('https://www.whoscored.com/Regions/206/Tournaments/4/Seasons/1929/Sta
 
 
 # accept cookies
-WebDriverWait(driver, 5)\
+WebDriverWait(driver, 100)\
     .until(EC.element_to_be_clickable((By.CSS_SELECTOR,
                                       'button.css-1wc0q5e'.replace(' ', '.'))))\
     .click()
+    
+# all players
+WebDriverWait(driver, 100)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '//*[@id="apps"]/dd[2]/a'.replace(' ', '.'))))\
+    .click()
+
+
 
 # players table
 all_summary = get_tables(driver,'//div[@id="statistics-table-summary"]','//div[@id="statistics-paging-summary"]//a[@id="next"]')
 WebDriverWait(driver, 1000000000)\
             .until(EC.element_to_be_clickable((By.XPATH,
                                                 '//div[@id="statistics-table-summary"]')))
+            
 
+
+#get last player
 table_columns = driver.find_element_by_xpath('//div[@id="statistics-table-summary"]')
 table = table_columns.text
 all_summary.append(table)
+
 #defensive
-# WebDriverWait(driver, 10)\
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-defensive"]'.replace(' ', '.'))))\
-#     .click()
+WebDriverWait(driver, 10)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-defensive"]'.replace(' ', '.'))))\
+    .click()
+
+# all players
+WebDriverWait(driver, 100)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '/html/body/div[5]/div[5]/div[3]/div[1]/div[2]/dl[2]/dd[2]/a'.replace(' ', '.'))))\
+    .click()
 
 
-# all_defensive = get_tables(driver,'//div[@id="statistics-table-defensive"]',
-#                            '//div[@id="statistics-paging-defensive"]//a[@id="next"]',
-#                            '//div[@id="statistics-paging-defensive"]//dl[@class="listbox right"]//b')
+all_defensive = get_tables(driver,'//div[@id="statistics-table-defensive"]',
+                            '//div[@id="statistics-paging-defensive"]//a[@id="next"]',
+                            '//div[@id="statistics-paging-defensive"]//dl[@class="listbox right"]//b')
 
-# #offensive
-# WebDriverWait(driver, 10)\
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-offensive"]'.replace(' ', '.'))))\
-#     .click()
+#get last player
+table_columns = driver.find_element_by_xpath('//div[@id="statistics-table-defensive"]')
+table = table_columns.text
+all_defensive.append(table)
+
+
+#offensive
+WebDriverWait(driver, 10)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-offensive"]'.replace(' ', '.'))))\
+    .click()
+
+# all players
+WebDriverWait(driver, 100)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '/html/body/div[5]/div[5]/div[4]/div[1]/div[2]/dl[2]/dd[2]/a'.replace(' ', '.'))))\
+    .click()
+
+all_offensive = get_tables(driver,'//div[@id="statistics-table-offensive"]',
+                            '//div[@id="statistics-paging-offensive"]//a[@id="next"]',
+                            '//div[@id="statistics-paging-offensive"]//dl[@class="listbox right"]//b')
+
+#get last player
+table_columns = driver.find_element_by_xpath('//div[@id="statistics-table-offensive"]')
+table = table_columns.text
+all_offensive.append(table)
+
+#passing
+WebDriverWait(driver, 5)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-passing"]'.replace(' ', '.'))))\
+    .click()
     
+# all players
+WebDriverWait(driver, 100)\
+    .until(EC.element_to_be_clickable((By.XPATH,
+                                      '/html/body/div[5]/div[5]/div[5]/div[1]/div[2]/dl[2]/dd[2]/a'.replace(' ', '.'))))\
+    .click()
 
-# all_offensive = get_tables(driver,'//div[@id="statistics-table-offensive"]',
-#                            '//div[@id="statistics-paging-offensive"]//a[@id="next"]',
-#                            '//div[@id="statistics-paging-offensive"]//dl[@class="listbox right"]//b')
 
-# #passing
-# WebDriverWait(driver, 5)\
-#     .until(EC.element_to_be_clickable((By.XPATH,
-#                                       '//li[@class="in-squad-detailed-view"]//a[@href="#stage-top-player-stats-passing"]'.replace(' ', '.'))))\
-#     .click()
-    
-# all_passing = get_tables(driver,'//div[@id="statistics-table-passing"]',
-#                            '//div[@id="statistics-paging-passing"]//a[@id="next"]',
-#                            '//div[@id="statistics-paging-passing"]//dl[@class="listbox right"]//b')
+all_passing = get_tables(driver,'//div[@id="statistics-table-passing"]',
+                            '//div[@id="statistics-paging-passing"]//a[@id="next"]',
+                            '//div[@id="statistics-paging-passing"]//dl[@class="listbox right"]//b')
+
+#get last player
+table_columns = driver.find_element_by_xpath('//div[@id="statistics-table-passing"]')
+table = table_columns.text
+all_offensive.append(table)
+
+
 
 
 ## Player creation
 
+summary_player = create_df_summary(all_summary)
+defensive_player = create_df_defensive(all_defensive)
+offensive_player = create_df_offensive(all_offensive)
+passing_player = create_df_passing(all_passing)
+        
+final = merge_all_dfs(summary_player, defensive_player, offensive_player, passing_player)
 
-summary_player = pd.DataFrame(columns=['name','club','position','apps','mins','goals','assists','yel','red','spg','ps%','aerials_won','motm','rating'])
-for x in all_summary:
-    try:
-        players = x.split('\n')
-        players_num = int(len(players))
-        players_num = math.floor((players_num / 4))
-        name_index = 3
-        for i in range(0,players_num):
-            name = players[name_index]
-            club = players[name_index + 1].split(',')[0]
-            position = players[name_index + 1].split(',')[-1]
-            stats = players[name_index + 2].split(' ')
-            apps = stats[0]
-            mins = stats[1]
-            goals = stats[2]
-            assists = stats[3]
-            yel = stats[4]
-            red = stats[5]
-            spg = stats[6]
-            ps = stats[7]
-            aerials = stats[8]
-            motm = stats[9]
-            rating = stats[10]
-            
-            summary_p = {'name':name,'club':club,'position':position,'apps':apps,'mins':mins,'goals':goals,'assists':assists,'yel':yel,'red':red,'spg':spg,'ps%':ps,'aerials_won':aerials,'motm':motm,'rating':rating}
-            summary_player = summary_player.append(summary_p,ignore_index=True)
-            
-            name_index += 4 
-        
-        
-    except Exception as e:
-        print(e)
-    
-print(summary_player)    
+# get league and season
+league = driver.find_element_by_xpath('//*[@id="tournaments"]//option[@selected="selected"]').text
+season = driver.find_element_by_xpath('//*[@id="seasons"]//option[@selected="selected"]').text
+season1 = season.split("/")[0]
+season2 = season.split("/")[1]
+final.to_csv(league+season1+"-"+season2+".csv")
