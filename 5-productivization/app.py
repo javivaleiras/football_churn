a5d4f14972672f3595eaad4b951376cd1b0c8d79
javivaleiras <javivaleiras95@gmail.com>
2023-01-11 22:13:37 +0100

    Flask API done. Qcuts for apps remaining

--- 5-productivization/app.py
+++ 5-productivization/app.py
@@ -6,14 +6,18 @@ from flask import Flask, jsonify, request
 import json
 import pickle
 import numpy as np
-
+import joblib
 
 def load_models():
-    file_name = "models/model_file.p"
-    with open(file_name, 'rb') as pickled:
-        data = pickle.load(pickled)
-        model = data['model']
+    file_name = 'models/random_forest.joblib'
+    
+    model = joblib.load(file_name)
     return model
+    # file_name = "models/random_forest.pkl"
+    # with open(file_name, 'rb') as pickled:
+    #     data = pickle.load(pickled)
+    #     model = data['model']
+    # return model
 
 
 app = Flask(__name__)
@@ -21,14 +25,17 @@ app = Flask(__name__)
 @app.route('/predict', methods=['GET'])
 
 def predict():
-    # stub input features
-    data = request.get_json() 
-    # load model
-    print("ssssssssssssssssssssssssssssssss")
-    model = load_models()
-    prediction = model.predict(np.array(list(data.values())))
-    response = json.dumps({'response': prediction[0]})
+    request_json = request.get_json()
+    x = request_json['input']
     
+    # load model
+    model = load_models()
+    prediction = model.predict(x)
+    if prediction == 1:
+        answer = "yes, the player will leave the team"
+    else:
+        answer = "no, the player will not leave the team"
+    response = json.dumps({'response': answer})
     return response, 200
 
 
